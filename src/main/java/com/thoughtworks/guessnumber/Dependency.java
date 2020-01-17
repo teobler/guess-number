27bd2cd1aac347aa184b76c088759fb1054deaa0
linwenjun <linwenjun@163.com>
2020-01-17 09:52:25 +0000

    first commit

---
+++ src/main/java/com/thoughtworks/guessnumber/Dependency.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.guessnumber;
+
+public class Dependency {
+    public String say(){
+        return "Leave me alone.";
+    }
+}
---
+++ src/main/java/com/thoughtworks/guessnumber/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.guessnumber;
+
+public class HelloWorld {
+    private Dependency dependency;
+
+    public HelloWorld(Dependency dependency) {
+
+        this.dependency = dependency;
+    }
+    public String beenCalled() {
+        return dependency.say();
+    }
+}
