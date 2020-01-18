06c6f6c756d13bd2d50140d64a7f4b81e883ee52
teobler <teobler@163.com>
2020-01-18 22:29:14 +0800

    refactor - extract input formatter and positionAndNumberAllRight methods.

--- src/main/java/com/thoughtworks/guessnumber/InputHandler.java
+++ src/main/java/com/thoughtworks/guessnumber/InputHandler.java
@@ -5,22 +5,30 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class InputHandler {
+  OutputObject outputObject = new OutputObject();
 
   public OutputObject handle(String input, List<Integer> answer) {
-    List<Integer> inputNumbers = Arrays
+    List<Integer> inputNumbers = formatInputStringAsIntList(input);
+
+    handlePositionAndNumberAllRight(answer, inputNumbers);
+
+    return this.outputObject;
+  }
+
+  private List<Integer> formatInputStringAsIntList(String input) {
+    return Arrays
         .stream(input.split(" "))
         .mapToInt(Integer::new)
         .boxed()
         .collect(Collectors.toList());
-    OutputObject outputObject = new OutputObject();
+  }
 
+  private void handlePositionAndNumberAllRight(List<Integer> answer, List<Integer> inputNumbers) {
     for (int i = 0; i < inputNumbers.size(); i++) {
       if (inputNumbers.get(i).equals(answer.get(i))) {
-        outputObject.allRightNumberCount++;
-        outputObject.allRightNumbers.add(inputNumbers.get(i));
+        this.outputObject.allRightNumberCount++;
+        this.outputObject.allRightNumbers.add(inputNumbers.get(i));
       }
     }
-
-    return outputObject;
   }
 }
