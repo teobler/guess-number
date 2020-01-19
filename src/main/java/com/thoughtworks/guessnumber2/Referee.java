861d5ff069e91a5f98e6b62c1d01ad4bd65aaf33
teobler <teobler@163.com>
2020-01-19 22:21:31 +0800

    should return 0A1B given guess numbers match 1 correct number without position.

--- src/main/java/com/thoughtworks/guessnumber2/Referee.java
+++ src/main/java/com/thoughtworks/guessnumber2/Referee.java
@@ -3,7 +3,11 @@ package com.thoughtworks.guessnumber2;
 import java.util.List;
 
 public class Referee {
-  public String judge(String inputNumber, List<String> answer) {
-    return "0A0B";
+  Facade facade = new Facade();
+
+  public String judge(String input, List<String> answer) {
+    List<String> inputNumbers = facade.mapInputToList(input);
+    String positionCorrectCount = String.valueOf(inputNumbers.stream().filter(answer::contains).count());
+    return "0A" + positionCorrectCount + "B";
   }
 }
--- src/test/java/com/thoughtworks/guessnumber2/RefereeTest.java
+++ src/test/java/com/thoughtworks/guessnumber2/RefereeTest.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class RefereeTest {
   Referee referee;
@@ -29,19 +31,25 @@ public class RefereeTest {
     assertEquals(result, "0A0B");
   }
 
-//  @Test
-//  public void should_return_1A0B_given_guess_numbers_match_1_correct() {
-//    Facade facade = mock(Facade.class);
-//    ArrayList<String> inputNumbers = new ArrayList<String>() {{
-//      add("5");
-//      add("6");
-//      add("7");
-//      add("8");
-//    }};
-//    when(facade.mapInputToList("5 6 7 8")).thenReturn(inputNumbers);
-//
-//    String result = referee.judge("5 6 7 8", answer);
-//
-//    assertEquals(result, "1A0B");
-//  }
+  @Test
+  public void should_return_0A1B_given_guess_numbers_match_1_correct_number_without_position() {
+    List<String> answer = new ArrayList<String>() {{
+      add("1");
+      add("2");
+      add("3");
+      add("4");
+    }};
+    ArrayList<String> inputNumbers = new ArrayList<String>() {{
+      add("2");
+      add("6");
+      add("7");
+      add("8");
+    }};
+    Facade facade = mock(Facade.class);
+    when(facade.mapInputToList("2 6 7 8")).thenReturn(inputNumbers);
+
+    String result = referee.judge("2 6 7 8", answer);
+
+    assertEquals(result, "0A1B");
+  }
 }
