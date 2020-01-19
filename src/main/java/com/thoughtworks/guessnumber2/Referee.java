994e9b321085bfe9f2a23d08dc1b0257ddc3cb89
teobler <teobler@163.com>
2020-01-19 21:59:06 +0800

    Referee's judge method should receive a answer list.

--- src/main/java/com/thoughtworks/guessnumber2/Referee.java
+++ src/main/java/com/thoughtworks/guessnumber2/Referee.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.guessnumber2;
 
+import java.util.List;
+
 public class Referee {
-  public String judge(String inputNumber) {
+  public String judge(String inputNumber, List<String> answer) {
     return "0A0B";
   }
 }
--- src/test/java/com/thoughtworks/guessnumber2/RefereeTest.java
+++ src/test/java/com/thoughtworks/guessnumber2/RefereeTest.java
@@ -3,6 +3,9 @@ package com.thoughtworks.guessnumber2;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class RefereeTest {
@@ -15,8 +18,30 @@ public class RefereeTest {
 
   @Test
   public void should_return_0A0B_given_guess_numbers_match_nothing() {
-    String result = referee.judge("5 6 7 8");
+    List<String> answer = new ArrayList<String>() {{
+      add("1");
+      add("2");
+      add("3");
+      add("4");
+    }};
+    String result = referee.judge("5 6 7 8", answer);
 
     assertEquals(result, "0A0B");
   }
+
+//  @Test
+//  public void should_return_1A0B_given_guess_numbers_match_1_correct() {
+//    Facade facade = mock(Facade.class);
+//    ArrayList<String> inputNumbers = new ArrayList<String>() {{
+//      add("5");
+//      add("6");
+//      add("7");
+//      add("8");
+//    }};
+//    when(facade.mapInputToList("5 6 7 8")).thenReturn(inputNumbers);
+//
+//    String result = referee.judge("5 6 7 8", answer);
+//
+//    assertEquals(result, "1A0B");
+//  }
 }
