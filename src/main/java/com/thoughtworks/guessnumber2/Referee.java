7497e6591467d80a08b291f5865af96e79b68bc5
teobler <teobler@163.com>
2020-01-20 16:37:06 +0800

    refactor - rename input to inputStringNumber.

--- src/main/java/com/thoughtworks/guessnumber2/Referee.java
+++ src/main/java/com/thoughtworks/guessnumber2/Referee.java
@@ -5,17 +5,18 @@ import java.util.List;
 public class Referee {
   Facade facade = new Facade();
 
-  public String judge(List<String> answer, String input) {
-    List<String> inputNumbers = facade.mapInputToList(input);
+  public String judge(List<String> answer, String inputStringNumber) {
+    List<String> inputStringNumbers = facade.mapInputToList(inputStringNumber);
+
     String correctCount =
         String.valueOf(
-            inputNumbers.stream()
-                .filter(number -> answer.indexOf(number) == inputNumbers.indexOf(number))
+            inputStringNumbers.stream()
+                .filter(number -> answer.indexOf(number) == inputStringNumbers.indexOf(number))
                 .count());
     String positionCorrectCount =
         String.valueOf(
-            inputNumbers.stream()
-                .filter(number -> answer.indexOf(number) != inputNumbers.indexOf(number))
+            inputStringNumbers.stream()
+                .filter(number -> answer.indexOf(number) != inputStringNumbers.indexOf(number))
                 .filter(answer::contains)
                 .count());
 
