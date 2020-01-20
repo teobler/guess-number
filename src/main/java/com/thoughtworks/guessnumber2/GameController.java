b5e5fb1006c84e9ddc2945134e891a57f3e3b361
teobler <teobler@163.com>
2020-01-20 16:55:37 +0800

    should return all previous result given wrong guessing twice.

--- src/main/java/com/thoughtworks/guessnumber2/GameController.java
+++ src/main/java/com/thoughtworks/guessnumber2/GameController.java
@@ -1,16 +1,19 @@
 package com.thoughtworks.guessnumber2;
 
+import java.util.ArrayList;
 import java.util.List;
-
 
 public class GameController {
   Referee referee;
+  List<String> result = new ArrayList<>();
 
   public GameController(Referee referee) {
     this.referee = referee;
   }
 
-  public String startNewRound(List<String> answer, String inputNumber) {
-    return this.referee.judge(answer, inputNumber);
+  public List<String> startNewRound(List<String> answer, String inputNumber) {
+    result.add(referee.judge(answer, inputNumber));
+
+    return this.result;
   }
 }
--- src/test/java/com/thoughtworks/guessnumber2/GameControllerTest.java
+++ src/test/java/com/thoughtworks/guessnumber2/GameControllerTest.java
@@ -1,30 +1,59 @@
 package com.thoughtworks.guessnumber2;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class GameControllerTest {
-  @Test
-  public void should_return_guessing_result_given_first_guessing() {
-    List<String> answer = new ArrayList<String>() {{
+  List<String> answer;
+
+  @Before
+  public void setUp() {
+    answer = new ArrayList<String>() {{
       add("1");
       add("2");
       add("3");
       add("4");
     }};
+  }
+
+  @Test
+  public void should_return_guessing_result_given_first_guessing() {
     String inputStringNumber = "1 5 6 7";
     Referee referee = mock(Referee.class);
     when(referee.judge(answer, inputStringNumber)).thenReturn("1A0B");
     GameController gameController = new GameController(referee);
 
-    String result = gameController.startNewRound(answer, inputStringNumber);
+    List<String> result = gameController.startNewRound(answer, inputStringNumber);
 
-    assertEquals("1A0B", result);
+    assertEquals("1A0B", result.get(0));
+  }
+
+  @Test
+  public void should_return_all_previous_result_given_wrong_guessing_twice() {
+    String firstInputStringNumber = "1 5 6 7";
+    String secondInputStringNumber = "1 2 6 7";
+    List<String> inputStringNumbers = new ArrayList<String>() {{
+      add(firstInputStringNumber);
+      add(secondInputStringNumber);
+    }};
+    Referee referee = mock(Referee.class);
+    when(referee.judge(answer, firstInputStringNumber)).thenReturn("1A0B");
+    when(referee.judge(answer, secondInputStringNumber)).thenReturn("2A0B");
+    GameController gameController = new GameController(referee);
+    AtomicReference<List<String>> result = new AtomicReference<>();
+
+    inputStringNumbers.forEach(numbers -> result.set(gameController.startNewRound(answer, numbers)));
+
+    assertEquals(2, result.get().size());
+    assertEquals("1A0B", result.get().get(0));
+    assertEquals("2A0B", result.get().get(1));
   }
 }
