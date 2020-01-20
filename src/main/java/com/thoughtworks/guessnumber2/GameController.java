209a78a06b3e2a8720b9bfc26bfb3517ff54b483
teobler <teobler@163.com>
2020-01-20 17:20:11 +0800

    refactor - remove all GameController code.

--- src/main/java/com/thoughtworks/guessnumber2/GameController.java
+++ src/main/java/com/thoughtworks/guessnumber2/GameController.java
@@ -1,19 +1,5 @@
 package com.thoughtworks.guessnumber2;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class GameController {
-  Referee referee;
-  List<String> result = new ArrayList<>();
 
-  public GameController(Referee referee) {
-    this.referee = referee;
-  }
-
-  public List<String> startNewRound(List<String> answer, String inputNumber) {
-    result.add(referee.judge(answer, inputNumber));
-
-    return this.result;
-  }
 }
